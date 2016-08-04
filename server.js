69945b5085cb91ee15ee326e989489b3159287e9
xthedeus <fhov@itu.dk>
2016-08-04 22:10:23 +0200

    renamed socket.js to server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var io = require('socket.io')();
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var count = 0;
 
 var port = process.argv[process.argv.length-1];
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
 
 io.on('connection', function(socket){
     console.log("new user_connected");
@@ -22,4 +31,4 @@ io.on('connection', function(socket){
 
 });
 
-io.listen(port);
+server.listen(port);
